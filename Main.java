7789fe98cbbab470fac2bb193071d3c0680bdaa2
alphanoobie <hussaingadi.hg@gmail.com>
2024-06-27 03:39:58 +0930

    create box

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        try (Scanner inputObj = new Scanner(System.in)) {
+            System.out.println("Enter Dimension");
+            int dimension = inputObj.nextInt();
+            String hashtag = "#";
+            String blank = " ";
+            for (int i = 0; i < dimension; i++) {
+                if (i == 0 || i == dimension - 1) {
+                    String line = hashtag.repeat(dimension);
+                    System.out.println(line);
+                } else {
+                    String line = blank.repeat(dimension - 2);
+                    System.out.println(hashtag + line + hashtag);
+                }
+            }
+        }
+    }
+}
